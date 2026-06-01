9ad9a3b514ac9b079c41204bce0eabc5cc6c49b4
Bajol55 <devarafadil35@gmail.com>
2026-06-01 12:34:03 +0700

    imperatif commit

---
+++ ioFileWithPath.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    
+    //deklarasi variabel baris dan NamaFile
+    string baris, NamaFile;
+    
+    //membuat input untuk menangkap nama file
+    cout << "Masukkan nama file : ";
+    cin >> NamaFile;
+    
+    
